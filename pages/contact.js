aafcf1c2abfc7c4140d63bf0fdb2f8e13024ab5b
jpoveda22 <47684055+jpoveda22@users.noreply.github.com>
2020-04-12 15:06:29 -0600

    installed plugins for blog posts

---
+++ pages/contact.js
@@ -0,0 +1,25 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const ContactPage = () => {
+  return (
+    <Layout>
+      <h1>Find me</h1>
+      <p>
+        <a href="https://soundcloud.com/mtphyscs" target="_blank">
+          SoundCloud
+        </a>
+        ,{" "}
+        <a href="https://twitter.com/mtphyscs" target="_blank">
+          Twitter
+        </a>
+        ,{" "}
+        <a href="https://www.instagram.com/mtphyscs/" target="_blank">
+          Instagram
+        </a>
+      </p>
+    </Layout>
+  )
+}
+
+export default ContactPage
