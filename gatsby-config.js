639132702f07f1767e31dd794def9bc32f4a5915
jpoveda22 <47684055+jpoveda22@users.noreply.github.com>
2020-04-12 20:50:52 -0600

    new plugins tested and working

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,36 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: []
+  siteMetadata: {
+    title: 'mtphyscs',
+    author: 'mtphyscs'
+  },
+
+  plugins: [
+    'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'src',
+        path: `${__dirname}/src/`
+      }
+    },
+    'gatsby-plugin-sharp',
+    {
+      resolve:'gatsby-transformer-remark',
+      options:{
+        plugins:[
+          'gatsby-remark-relative-images',
+          {
+            resolve:'gatsby-remark-images',
+            options:{
+              maxWidth: 750,
+              linkImagesToOriginal: false,
+            }
+          }
+         
+
+
+        ]
+      }
+    }
+  ]
 }
