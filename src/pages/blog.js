b3635e6675eb720ff8b9d40e4a5a8f5d126b7c5a
jpoveda22 <47684055+jpoveda22@users.noreply.github.com>
2020-04-12 22:06:52 -0600

    added more sass and blog is now working

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,8 +1,12 @@
 import React from "react"
-import { useStaticQuery, graphql } from 'gatsby'
+import { Link, useStaticQuery, graphql } from 'gatsby'
+import blogStyles from './blog.module.scss'
 
 
 import Layout from "../components/layout"
+
+
+// MARKDOWN BLOG
 
 const BlogPage = () => {
   const data = useStaticQuery(graphql`
@@ -14,6 +18,9 @@ const BlogPage = () => {
               title
               date
             }
+            fields{
+              slug
+            }
           }
         }
       }
@@ -24,12 +31,15 @@ const BlogPage = () => {
   return (
     <Layout>
       <h1>Blog</h1>
-     <ol>
+     <ol className={blogStyles.posts}>
        {data.allMarkdownRemark.edges.map((edge)=>{
          return(
-           <li>
-             <h2>{edge.node.frontmatter.title}</h2>
+           <li className={blogStyles.post}>
+             <Link to={`/blog/${edge.node.fields.slug}`}>
+             <h2>{edge.node.frontmatter.title}</h2> 
              <p>{edge.node.frontmatter.date}</p>
+            
+             </Link>
            </li>
          )
 
