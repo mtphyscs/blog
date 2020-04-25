defbbe7255a5b879c1eb345273905492b0bac4d5
形而上学 <47684055+mtphyscs@users.noreply.github.com>
2020-04-25 17:15:23 -0600

    added Amazon wishlist plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,36 +1,51 @@
 module.exports = {
   siteMetadata: {
-    title: 'mtphyscs',
-    author: 'mtphyscs'
+    title: "mtphyscs",
+    author: "mtphyscs",
   },
 
   plugins: [
-    'gatsby-plugin-sass',
+    "gatsby-plugin-react-helmet",
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: '@sbardian/gatsby-source-amazon-wishlist',
       options: {
-        name: 'src',
-        path: `${__dirname}/src/`
-      }
-    },
-    'gatsby-plugin-sharp',
-    {
-      resolve:'gatsby-transformer-remark',
-      options:{
-        plugins:[
-          'gatsby-remark-relative-images',
+        lists: [
           {
-            resolve:'gatsby-remark-images',
-            options:{
+            owner: 'mtphyscs',
+            wishlistUrl: 'https://www.amazon.com/hz/wishlist/ls/2LUM7XCL2UCCV/ref=nav_wishlist_lists_2?_encoding=UTF8&type=wishlist',
+            limit: false,
+          },
+          {
+            owner: 'mtphyscs',
+            wishlistUrl: 'https://www.amazon.com/hz/wishlist/ls/2LUM7XCL2UCCV/ref=nav_wishlist_lists_2?_encoding=UTF8&type=wishlist',
+          }
+        ]
+      },
+    },
+ 
+    "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "src",
+        path: `${__dirname}/src/`,
+      },
+    },
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          "gatsby-remark-relative-images",
+          {
+            resolve: "gatsby-remark-images",
+            options: {
               maxWidth: 750,
               linkImagesToOriginal: false,
-            }
-          }
-         
-
-
-        ]
-      }
-    }
-  ]
+            },
+          },
+        ],
+      },
+    },
+  ],
 }
