bbce091ae9864711e6a101d2e9ceeef2fef78672
形而上学 <47684055+mtphyscs@users.noreply.github.com>
2020-04-26 14:47:59 -0600

    Add files via upload

--- src/pages/library.js
+++ src/pages/library.js
@@ -1,54 +1,48 @@
 import React from "react"
 import Layout from "../components/layout"
-import { Link, useStaticQuery, graphql } from 'gatsby'
-import Head from '../components/head'
-import libraryStyles from './library.module.scss'
+import { useStaticQuery, graphql } from "gatsby"
+import Head from "../components/head"
+import libraryStyles from "./library.module.scss"
 
 const Library = () => {
   const data = useStaticQuery(graphql`
-    query {
-      allAmazonWishlistItem {
-        edges {
-          node {
-            title
+  {
+    allAmazonWishlistItem {
+      edges {
+        node {
+          title
+          image {
             url
-            features
-            comment
-            image {
-              url
-            }
           }
         }
       }
     }
+  }
   `)
 
-
-
- 
   return (
     <Layout>
       <Head title="Library" />
       <h1>Library</h1>
-      <h2>Here you’ll find an occasionally-updated list of books I’ve read and enjoyed.</h2>
+      <h2>
+        Here you’ll find an occasionally-updated list of books I’ve read and
+        enjoyed.
+      </h2>
       <ol>
-       {data.allAmazonWishlistItem.edges.map((edge)=>{
-         return(
-           <li>
-             <Link to={`/book/${edge.node.title}`}>
-             <img src={edge.node.image.url.replace("SS135_.jpg", "LL135_.jpg")}></img>
-             <h2 className={libraryStyles.a}>{edge.node.title}</h2> 
-          
-             
-            
-            
-             </Link>
-           </li>
-         )
-
-       })}
-     </ol>
-      
+        {data.allAmazonWishlistItem.edges.map(edge => {
+          return (
+            <li>
+              {/* <Link to={`/book/${edge.node.title}`}> */}
+              <img
+                src={edge.node.image.url.replace("SS135_.jpg", "LL135_.jpg")}
+                alt=""
+              ></img>
+              <p className={libraryStyles.a}>{edge.node.title}</p>
+              {/* </Link> */}
+            </li>
+          )
+        })}
+      </ol>
     </Layout>
   )
 }
